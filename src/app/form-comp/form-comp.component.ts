cf742e60bf43439407ad91e3fffd71f26d3146ad
Michal Drobnik <drobnikmi@wp.pl>
2018-03-03 12:12:46 +0100

    update form

--- src/app/form-comp/form-comp.component.ts
+++ src/app/form-comp/form-comp.component.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-comp',
   template: `
-  <form [formGroup]="someForm" (ngSubmit)="">
-  <div class="form-group">
-    <label class="center-block">Name:
-      <input class="form-control" formControlName="name">
-    </label>
-  </div>
-  <button type="submit">someButton</button>
+  <form [formGroup]="myForm" (ngSubmit)="onSubmit(myForm)">
+  <input formControlName="name">
+  <button type="submit">Send</button>
 </form>
-<p>Form value: {{ someForm.value}}</p>
   `,
   styleUrls: ['./form.component.sass']
 })
 export class FormCompComponent implements OnInit {
-  someForm = new FormGroup ({
-    name: new FormControl()
-  });
+  myForm: FormGroup;
   constructor() { }
-
   ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl('')
+    });
   }
-
+  onSubmit(form: FormGroup) {
+    console.log('Name', form.value.name, [Validators.required, , Validators.minLength(1)]);
+  }
 }
