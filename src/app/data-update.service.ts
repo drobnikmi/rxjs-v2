7f15af8db4a18bf5befc2bb94674a45e59f691af
Mateo <mateok@onet.eu>
2018-04-23 22:44:20 +0200

    merge conflits

--- src/app/data-update.service.ts
+++ src/app/data-update.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
@@ -29,23 +28,5 @@ export class DataUpdateService {
   getUrl(id?: string): string {
     const mainId = id ? '/' + id : '';
     return `https://chat-devmeeting.firebaseio.com/messages${mainId}.json`;
-=======
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
-import { map, tap, catchError } from 'rxjs/operators';
-
-@Injectable()
-export class DataUpdateService {
-
-  constructor(private http: HttpClient) {
-  }
-  addElement(data){
-    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
-    return this.http.post(this.getUrl('zmienna'), data, headers);
-  }
-  getUrl(id?:string):string{
-    const mainId = id ? '/' + id : '';
-    return `https://chat-devmeeting.firebaseio.com/messages${mainId}.json`
->>>>>>> b17e26589dc7cdbe78f6fd5e22ae4d0b1c22687c
   }
 }
--- src/app/form-comp/form-comp.component.ts
+++ src/app/form-comp/form-comp.component.ts
@@ -12,7 +12,6 @@ import 'rxjs/add/operator/filter';
 @Component({
   selector: 'app-form-comp',
   template: `
-<<<<<<< HEAD
   <form class="mb-5" [formGroup]="myForm" (ngSubmit)="onSubmit()">
     <div class="form-group">
       <label class="center-block" for="userInput">User:</label>
@@ -25,22 +24,6 @@ import 'rxjs/add/operator/filter';
       </div>
     <button type="submit" [disabled]="myForm.invalid" class="btn btn-primary" >Send</button>
   </form>
-=======
-  <form [formGroup]="myForm" (ngSubmit)="formSubject.next()">
-  <div class="form-group">
-    <label class="center-block">Message:
-      <input class="form-control" formControlName="body">
-    </label>
-    <label class="center-block">Time:
-      <input class="form-control" formControlName="timestamp">
-    </label>
-    <label class="center-block">User:
-      <input class="form-control" formControlName="sender">
-    </label>
-  </div>
-  <button type="submit" [disabled]="myForm.invalid" >Send</button>
-</form>
->>>>>>> b17e26589dc7cdbe78f6fd5e22ae4d0b1c22687c
   `,
   styleUrls: ['./form.component.sass']
 })
@@ -53,26 +36,16 @@ export class FormCompComponent {
   constructor(private formBuilder: FormBuilder) {
     this.myForm = formBuilder.group({
       body: ['', Validators.required],
-<<<<<<< HEAD
-=======
-      timestamp: ['', Validators.required],
->>>>>>> b17e26589dc7cdbe78f6fd5e22ae4d0b1c22687c
       sender: ['', Validators.required]
     });
 
     this.formSubject
       .filter(() => this.myForm.valid)
-<<<<<<< HEAD
       .subscribe(() => this.formSubmit.emit(this.myForm.value));
   }
 
   onSubmit() {
     this.formSubject.next();
     this.myForm.reset();
-=======
-      // .map(() => this.form.value)
-      // .subscribe(this.formSubmit);
-      .subscribe(() => this.formSubmit.emit(this.myForm.value))
->>>>>>> b17e26589dc7cdbe78f6fd5e22ae4d0b1c22687c
   }
 }
